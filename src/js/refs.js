2f12aba7b9328d25ae00acec44299f3256d3f1c1
NikKms <mykola.kamyshnikov@gmail.com>
2023-05-15 14:05:44 +0300

    second fix deploy

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,5 @@
 export default {
   input: document.getElementById('search-box'),
   countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'), // create countryInfo element if it doesn't exist
+  countryInfo: document.querySelector('.country-info'),
 };
